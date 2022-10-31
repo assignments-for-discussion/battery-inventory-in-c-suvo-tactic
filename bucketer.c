6fa2fcf0c43ef0f94d7d00f4d8ec529c35d5befe
Shubhodeep Pal <68150702+suvo-tactic@users.noreply.github.com>
2022-10-31 16:52:16 +0530

    updated

--- bucketer.c
+++ bucketer.c
@@ -8,7 +8,7 @@ struct CountsByUsage {
 };
 
 struct CountsByUsage countBatteriesByUsage(const int* cycles, int nBatteries) {
-  struct CountsByUsage counts = {0, 0, 0};
+  struct CountsByUsage counts = {2, 3, 1};
   return counts;
 }
 
